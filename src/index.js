3976d52b8ed0b124d080b63c113d092043b35dce
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-21 14:26:26 +0700

    Practice Making Business Cards

--- src/index.js
+++ src/index.js
@@ -1,17 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <div className="top">
+        <Header />
+        <Identity />
+      </div>
+      <div className="bottom">
+        <Biodata />
+        <Highlight />
+      </div>
+    </div>
+  );
+}
+
+function SocialButtons({ icon }) {
+  return (
+    <button>
+      <i className={`fa ${icon}`}></i>
+    </button>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <div className="social-buttons">
+        <SocialButtons icon="fa-facebook" />
+        <SocialButtons icon="fa-twitter" />
+        <SocialButtons icon="fa-instagram" />
+      </div>
+      <div className="social-buttons right">
+        <SocialButtons icon="fa-linkedin" />
+        <SocialButtons icon="fa-github" />
+      </div>
+    </>
+  );
+}
+
+function Identity() {
+  return (
+    <div className="text">
+      <div className="name-wrapper">
+        <div className="name">Black Cat</div>
+      </div>
+      <div className="title">Meow Engineer</div>
+    </div>
+  );
+}
+
+function Biodata() {
+  return (
+    <div className="desc">
+      <p>
+        My name is <b>Black Cat</b> and I am an Engineer
+      </p>
+      <p>
+        To say that I love software is an understatement, I spend every working
+        hour building something fun & watching Tech Podcasts or attending
+        Meetups & Conference
+      </p>
+    </div>
+  );
+}
+
+function Badge({ text }) {
+  return (
+    <button>
+      <div className="height">{text}</div>
+    </button>
+  );
+}
+
+function Highlight() {
+  return (
+    <div className="buttons">
+      <Badge text="🎓 PhD" />
+      <Badge text="🚵‍♀️ Mountainers" />
+      <Badge text="🎲 3D Geek" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
